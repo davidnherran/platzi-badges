16b78cece70b5fed8ee50c280dc57f139f654724
davidnherran <davidnherran@gmail.com>
2020-07-22 22:36:17 -0500

    Header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import logo from "../../images/logo.svg"
-import "./Navbar.css"
+import React, { Component } from "react";
+import logo from "../../images/logo.svg";
+import "./Navbar.css";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div className="Navbar">
-                <a href="/">
-                    <img src={logo} alt="Logo"/>
-                    <span className="font-weight-light">Platzi</span>
-                    <span className="font-weight-bold">Conf</span>
-                </a>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Navbar">
+        <div className="container-fluid">
+          <a className="Navbar__brand" href="/">
+            <img className="Navbar__brand-logo" src={logo} alt="Logo" />
+            <span className="font-weight-light">Platzi</span>
+            <span className="font-weight-bold">Conf</span>
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import Navbar from "../components/navbar/Navbar.jsx"
+import header from "../images/badge-header.svg"
+import "./styles/BadgeNew.css"
 
 class BadgeNew extends Component {
     render() {
         return (
             <div>
                 <Navbar />
+                <div className="BadgeNew__hero">
+                <img className="img-fluid" src={header} alt="Hero"/>
+                </div>
             </div>
         )
     }
