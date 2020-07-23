db1b0c1694944aa56d0b722ae9e1e93a4818f68f
davidnherran <davidnherran@gmail.com>
2020-07-22 22:43:28 -0500

    BadgeNew

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Badge from "./components/badge/Badge";
 import BadgeNew from "./pages/BadgeNew"
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
@@ -8,13 +7,6 @@ import "./global.css";
 ReactDOM.render(
   <React.StrictMode>
     <BadgeNew />
-      {/*<Badge
-    firstName="David"
-    lastName="Herran"
-    jobTitle="Frontend Engineer"
-    tagName="davidnherran"
-    avatarURL="https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg"
-  />*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/BadgeNew.jsx
+++ src/pages/BadgeNew.jsx
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
-import Navbar from "../components/navbar/Navbar.jsx"
-import header from "../images/badge-header.svg"
-import "./styles/BadgeNew.css"
+import React, { Component } from "react";
+import Navbar from "../components/navbar/Navbar.jsx";
+import Badge from "../components/badge/Badge";
+import header from "../images/badge-header.svg";
+import "./styles/BadgeNew.css";
 
 class BadgeNew extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <div className="BadgeNew__hero">
-                <img className="img-fluid" src={header} alt="Hero"/>
-                </div>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Hero" />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="David"
+                lastName="Herran"
+                jobTitle="Frontend Engineer"
+                tagName="davidnherran"
+                avatarURL="https://icon-library.com/images/avatar-icon/avatar-icon-6.jpg"
+              />
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default BadgeNew
+export default BadgeNew;
